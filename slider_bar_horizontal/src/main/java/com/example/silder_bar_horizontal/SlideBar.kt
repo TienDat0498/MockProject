8b4d5a43e583e910cb4f438baffe37ba42e5efe3
DatKieu <tiendat0498@gmail.com>
2022-01-05 13:35:34 +0700

    Initialize

---
+++ slider_bar_horizontal/src/main/java/com/example/silder_bar_horizontal/SlideBar.kt
@@ -0,0 +1,142 @@
+package com.example.slidebar
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.withStyledAttributes
+import com.example.silder_bar_horizontal.R
+
+
+internal class SlideBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    companion object {
+        const val GAP_VALUE: Int = 10
+        private const val STROKE_WIDTH = 4f
+    }
+
+
+    var moneyValue: Float = 1024f
+    var gapSize: Float = 24f
+        set(value) {
+            field = value
+            shortBar = gapSize / 8
+            longBar = gapSize / 2
+        }
+    private var shortBar: Float = gapSize / 8
+    private var longBar: Float = gapSize / 2
+    var startPadding: Float = 80f
+    var barColor: Int = Color.WHITE
+        set(value) {
+            field = value
+            paint.color = value
+        }
+
+
+    private val paint = Paint().apply {
+        color = barColor
+        isAntiAlias = true
+        style = Paint.Style.STROKE
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.SlideBar) {
+            moneyValue = getFloat(R.styleable.SlideBar_moneyValueSlideBar, 1024f)
+            gapSize = getDimension(R.styleable.SlideBar_gapSizeSlideBar, 24f)
+            startPadding = getDimension(R.styleable.SlideBar_startPaddingSlideBar, 80f)
+            barColor = getColor(R.styleable.SlideBar_barColorSlideBar, Color.WHITE)
+        }
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        var xDimen = startPadding
+        for (i in 0..(moneyValue / GAP_VALUE).toInt()) {
+            if (i % 5 == 0)
+                canvas.drawLine(
+                    xDimen,
+                    (height - longBar) / 2,
+                    xDimen,
+                    (height + longBar) / 2,
+                    paint
+                )
+            else
+                canvas.drawLine(
+                    xDimen,
+                    (height - shortBar) / 2,
+                    xDimen,
+                    (height + shortBar) / 2,
+                    paint
+                )
+            xDimen += gapSize
+
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+//        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+
+        val width: Int
+        val height: Int
+
+        //Measure Width
+        when(widthMode){
+            MeasureSpec.EXACTLY -> {
+                // For further implementation, complete this path, by default
+                // only use self define height and width
+                throw  NotImplementedError(
+                    "Not support this width mode, only support " +
+                            "wrap_content of horizontal scroll view"
+                )
+            }
+            MeasureSpec.AT_MOST -> {
+                // For further implementation, complete this path, by default
+                // only use self define height and width
+                throw  NotImplementedError(
+                    "Not support this width mode, only support " +
+                            "wrap_content of horizontal scroll view"
+                )
+            }
+            else -> //Be whatever you want
+                width = widthSize + (moneyValue / GAP_VALUE * gapSize).toInt()
+        }
+
+        //Measure Height
+        when(heightMode){
+            MeasureSpec.EXACTLY -> {
+                // For further implementation, complete this path, by default
+                // only use self define height and width
+                throw  NotImplementedError(
+                    "Not support this height mode, only support " +
+                            "wrap_content of horizontal scroll view"
+                )
+            }
+            MeasureSpec.AT_MOST-> {
+                // for using in horizontal scroll view, mode will be at most of height
+                height = (gapSize * 6).toInt()
+            }
+
+            else -> {
+                // For further implementation, complete this path, by default
+                // only use self define height and width
+                throw  NotImplementedError(
+                    "Not support this height mode, only support " +
+                            "wrap_content of horizontal scroll view"
+                )
+            }
+        }
+        setMeasuredDimension(width, height)
+    }
+}
